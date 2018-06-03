5ca5eb5fd35f8e46f5a86507a2ea98039a4c785f
Ben Feeser <benjamin.r.feeser@gmail.com>
2018-06-03 16:48:23 -0400

    db/dump fixes

--- db/dump.sh
+++ db/dump.sh
@@ -1,12 +1,16 @@
-DUMP=mysqldump --defaults-file=db/config
+#!/usr/bin/env sh
+DUMP="mysqldump --defaults-file=db/config"
 
-SCHEMAS\=(
+SCHEMAS=(
     sentinel
 )
 
 echo "Dumping schemas from $SCHEMAS into db/schema.sql"
 
-DUMP --no-data --databases $SCHEMAS --result-file=schema.sql
+$DUMP --no-data --databases $SCHEMAS --result-file=schema.sql
+
+# remove AUTO_INCREMENT
+sed -i '' 's/ AUTO_INCREMENT=[0-9]*\b//g' schema.sql
 
 TABLES=(
     hosts
@@ -18,4 +22,4 @@ TABLES=(
 
 echo "Dumping data from $TABLES into db/data.sql"
 
-DUMP --no-create-info $TABLES --result-file=data.sql
+$DUMP --no-create-info $TABLES --result-file=data.sql
