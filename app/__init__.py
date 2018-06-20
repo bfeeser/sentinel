db46748463175a3ec8c31034cd25b1a670b0ad53
Ben Feeser <benjamin.r.feeser@gmail.com>
2018-06-19 21:30:39 -0400

    better asset handling: compress and minify

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,46 @@
 from flask import Flask
+from flask_assets import Environment, Bundle
 from flask_caching import Cache
+from flask_compress import Compress
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config.from_pyfile("config.py")
 
-# instantiate cache for session memoization
+# gzip requests
+compress = Compress(app)
+
+# session memoization
 cache = Cache(app, config={"CACHE_TYPE": "simple"})
-cache.init_app(app)
 
-# initialize cryptography
+# cryptography
 bcrypt = Bcrypt(app)
 
-# initialize Login Manager
-login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 login_manager.next_url = None
 
-# get database configuration
-app.config.from_pyfile("config.py")
+assets = Environment(app)
+
+js = Bundle(
+    "js/jquery-2.1.3.min.js",
+    "js/bootstrap.min.js",
+    "js/bootstrap-table.min.js",
+    "js/scripts.js",
+    filters="jsmin",
+    output="gen/all.js",
+)
+assets.register("all_js", js)
+
+css = Bundle(
+    "css/bootstrap.min.css",
+    "css/bootstrap-table.min.css",
+    "css/style.css",
+    filters="cssmin",
+    output="gen/all.css",
+)
+assets.register("all_css", css)
 
 from app import views, models
